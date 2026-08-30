5aad909c3ce4daf415fc0eacbaaa459e7ddf9d7c
David-G-Cotom <brandoncotom202231460@cunoc.edu.gt>
2026-08-30 01:17:26 -0600

    Creacion del componente que muestra una tabla con todos los socios registrados.

--- frontend-gym/src/app/features/socios/components/lista-socios/lista-socios.component.ts
+++ frontend-gym/src/app/features/socios/components/lista-socios/lista-socios.component.ts
@@ -1,9 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { SociosService } from '../../services/socios.service';
+import { DatePipe, NgClass } from '@angular/common';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { catchError } from 'rxjs/internal/operators/catchError';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-lista-socios',
-  imports: [],
+  imports: [NgClass, DatePipe],
   templateUrl: './lista-socios.component.html',
   styleUrl: './lista-socios.component.css',
 })
-export class ListaSocios {}
+export class ListaSocios {
+
+  // Variables de estado de la tabla de socios
+  public errorCarga: boolean = false;
+
+  private sociosService = inject(SociosService);
+
+  public sociosSignal = toSignal(
+    this.sociosService.getSocios().pipe(
+      catchError((error) => {
+        console.error('Error en la carga de socios:', error);
+        this.errorCarga = true;
+        return of([]); // Retorna arreglo vacío en caso de fallo
+      })
+    )
+  );
+
+  // Helper para asignar colores dinámicos a los badges de Bootstrap
+  public obtenerEstiloBadge(estado: string): string {
+    const normalizado = estado.toLowerCase().trim();
+    if (normalizado.includes('activo')) return 'bg-success';
+    if (normalizado.includes('pendiente')) return 'bg-warning text-dark';
+    return 'bg-danger';
+  }
+
+}
